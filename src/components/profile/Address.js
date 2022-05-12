ec1003fbd0e16d6902bec3ca1938359dc50127c4
Khalil T laptop <tl.khalil@gmail.com>
2022-05-12 20:41:21 +0100

    work done

---
+++ src/components/profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Address = () =>{
+    return (
+        <div>
+            <h1 id="addr">Last seen <span>somewhere</span>, <span>everywhere</span><span></span></h1>
+        </div>
+    );
+}
+export default Address;
---
+++ src/components/profile/FullName.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const FullName = () =>{
+    return (
+        <div>
+            <h1><span>Foulen</span> B. <span>Falten</span></h1>
+        </div>
+    );
+}
+export default FullName;
